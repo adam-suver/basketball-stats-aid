d2a808dcb676eeb250700f5e0ba93909cc52c190
Adam Suver <asuver4@gmail.com>
2023-09-11 14:08:47 -0400

    Reconfigured database

--- java/database/create.sh
+++ java/database/create.sh
@@ -1,7 +1,8 @@
-export PGPASSWORD='dIOWrqhOP41Y3J3dkqcz'
+export PGPASSWORD='ZFBd0MXh5r6ZKfe0hKZE'
 DATABASE=railway
+BASEDIR=$(dirname $0)
 
-psql -h containers-us-west-150.railway.app -U postgres -p 7885 -d railway -f "$BASEDIR/dropdb.sql" &&
-psql -h containers-us-west-150.railway.app -U postgres -p 7885 -d railway -f "$BASEDIR/schema.sql" &&
-psql -h containers-us-west-150.railway.app -U postgres -p 7885 -d railway -f "$BASEDIR/data.sql" &&
-psql -h containers-us-west-150.railway.app -U postgres -p 7885 -d railway -f "$BASEDIR/user.sql"
+psql -h containers-us-west-58.railway.app -U postgres -p 5658 -d railway -f "$BASEDIR/dropdb.sql" &&
+psql -h containers-us-west-58.railway.app -U postgres -p 5658 -d railway -f "$BASEDIR/schema.sql" &&
+psql -h containers-us-west-58.railway.app -U postgres -p 5658 -d railway -f "$BASEDIR/data.sql" &&
+psql -h containers-us-west-58.railway.app -U postgres -p 5658 -d railway -f "$BASEDIR/user.sql"
